684c96adb499ebd765d0ff0ef71799266a009110
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-16 18:19:07 -0700

    [City_loop.py] add limit to number of cities tested in function to save time

--- City_loop.py
+++ City_loop.py
@@ -2,7 +2,7 @@ from Major_cities import major_cities
 from Doordash import doordash
 
 def cities():
-    for cities in major_cities:
-        print(cities)
+    for cities in major_cities[:10]:
+        doordash(cities)
 
 cities()
