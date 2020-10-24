d938d2655123f2d31400df7e29e1e4662e8a0531
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-24 14:47:52 -0700

    [server.js]remove js from app.use

--- routes/api/bigmacs.js
+++ routes/api/bigmacs.js
@@ -13,5 +13,18 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.post('/', (req, res) => {
+    const newBigMac = new BigMac({
+        city: req.body.city,
+        state: req.body.state,
+        price: req.body.price
+    })
+    newBigMac.save()
+    .then(createdBigMac => {
+        res.json(createdBigMac)
+    })
+    .catch(err => console.log(err))
+})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 8000;
 
-const bigmacs = require('./routes/api/bigmacs.js');
+const bigmacs = require('./routes/api/bigmacs');
 
 // MIDDLEWARE
 app.use(cors());
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.status(200).json({message: 'Smile, you are being watching by the Backend Team'});
 });
 
-app.use('/api/bigmacs.js', bigmacs)
+app.use('/api/bigmacs', bigmacs)
 
 app.listen(port, () => {
     console.log(`👀 🥁 Server is running on port: ${port}`);
